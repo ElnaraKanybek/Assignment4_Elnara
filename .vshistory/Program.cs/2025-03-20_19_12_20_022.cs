84ab98f27b7e92429428b8a4e973cc4a6c335d43
Elnara_Kanybek <elnarakanybek@gmail.com>
2025-03-21 08:59:32 -0700

    Add project files.

---
+++ .vshistory/Program.cs/2025-03-20_19_12_20_022.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment4_Elnara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+
+Elnara Assignment 4 Git repository link:
+
+https://github.com/ElnaraKanybek/ElnaraAssignment4.git
